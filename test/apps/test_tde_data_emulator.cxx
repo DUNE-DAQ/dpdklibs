c9f643efa78981ff480f909f94c3a7fc094cd4cd
Danaisis Vargas Oliva <dvargas@lxplus732.cern.ch>
2022-04-21 19:55:51 +0200

    fixing the creation of tde data files on emulator

--- test/apps/test_tde_data_emulator.cxx
+++ test/apps/test_tde_data_emulator.cxx
@@ -23,9 +23,8 @@ static constexpr int adc_set_samples = 8;
 static constexpr int bits_per_adc = 12;
 static constexpr int bits_per_word = 3 * sizeof(uint32_t);
 static constexpr int num_adc_words = tot_adc_12bits_samples * bits_per_adc / bits_per_word;
-static constexpr int rate_12bits = 3054;  // in Hz = 3.054 in ms
-static constexpr int rate_16bits = 2290; // in Hz = 2.290 in ms
-const uint64_t toptime = 0x222211111111;
+static constexpr int rate_12bits = 328;  // in Hz = 3.054 in ms
+static constexpr int rate_16bits = 437;  // in Hz = 2.290 in ms
 
 struct TDEHeader
 {
@@ -57,7 +56,7 @@ struct ADCData12
   uint32_t samples_info12bits[num_adc_words];
 };
 
-int main(int argc, char *argv[])
+int main()
 {
   TDEHeader tdeheader;
   ADCData16 adcdata16;
@@ -65,7 +64,10 @@ int main(int argc, char *argv[])
 
   FILE *fp_16, *fp_12;
 
+  bool killswitch { true };
   uint16_t new_adc_val = 0x9;
+  uint64_t toptime = 0x23;
+  uint64_t time = 0;
 
   // Seting the header
   tdeheader.version = 0x1;
@@ -104,44 +106,43 @@ int main(int argc, char *argv[])
 
   // using the RateLimiter in Hz
   auto limiter = RateLimiter(rate_16bits); 
-
-  // initializing the RateLimiter and t
   limiter.init();
-  for(uint64_t i = 0; i < 100; i++)
+
+  cout << "Seting timestamp" << endl;
+  while (killswitch) 
   {
-    while(i < toptime)
+    for(uint64_t i = 0; i < toptime; i++)
     {
-      tdeheader.timestamp = i;
+      time += i;
+      tdeheader.timestamp = time;
       limiter.limit();
-    }
-    
-    std::string str = std::to_string(i);
-    std::string filename16 = "/eos/home-d/dvargas/Salidadune/filesTDE16bits/binary_16bits_" + str + ".txt";
-    std::string filename12 = "/eos/home-d/dvargas/Salidadune/filesTDE12bits/binary_12bits_" + str + ".txt";
 
-    fp_16 = fopen(filename16.c_str(), "wb");
-    if (fp_16 == NULL) {
-        cout << "Could not open "
-             << "output file fp_16" << endl;
-        return -1;
+      std::string str = std::to_string(i);
+      std::string filename16 = "/eos/home-d/dvargas/Salidadune/filesTDE16bits/binary_16bits_" + str + ".bit";
+      std::string filename12 = "/eos/home-d/dvargas/Salidadune/filesTDE12bits/binary_12bits_" + str + ".bit";
+
+      cout << "Crating file binary_16bits_" << i << endl;
+      fp_16 = fopen(filename16.c_str(), "wb");
+      if (fp_16 == NULL) { cout << "Could not open output file fp_16" << endl; }
+      fwrite(&tdeheader, sizeof(tdeheader), 1, fp_16);
+      fwrite(&adcdata16, sizeof(adcdata16), 1, fp_16);
+      fclose(fp_16);
+      cout << "Closing file number binary_16bits_" << i << endl;
+
+      cout << "Crating file binary_12bits_" << i << endl;
+      fp_12 = fopen(filename12.c_str(), "wb");
+      if (fp_12 == NULL) { cout << "Could not open output file fp_12" << endl; }
+      fwrite(&tdeheader, sizeof(tdeheader), 1, fp_12);
+      fwrite(&adcdata12, sizeof(adcdata12), 1, fp_12);
+      fclose(fp_12);
+      cout << "Closing file number binary_12bits_" << i << endl;
     }
 
-    fwrite(&tdeheader, sizeof(tdeheader), 1, fp_16);
-    fwrite(&adcdata16, sizeof(adcdata16), 1, fp_16);
-    fclose(fp_16);
-    
-    fp_12 = fopen(filename12.c_str(), "wb");
-    if (fp_12 == NULL) {
-        cout << "Could not open "
-             << "output file fp_12" << endl;
-        return -1;
-    }
-    fwrite(&tdeheader, sizeof(tdeheader), 1, fp_12);
-    fwrite(&adcdata12, sizeof(adcdata12), 1, fp_12);
-    fclose(fp_12);
-    
-    //----------------------------------------------------------------------
+    killswitch = false;
+
   }
+
+  cout << "Finish while stamenet" << endl;
 
   return 0;
 }
