3a9e37c720bad06391b01d925d68bffb666d3734
jmcarcell <jmcarcell@users.noreply.github.com>
2022-05-10 10:31:10 +0200

    Add skeleton for the sender and receiver plugins

---
+++ plugins/NICReceiver.cpp
@@ -0,0 +1,92 @@
+/**
+ * @file NICReceiver.cpp NICReceiver DAQModule implementation
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+#include "dpdklibs/nicreader/Nljs.hpp"
+
+#include "NICReceiver.hpp"
+#include "dpdklibs/AvailableCallbacks.hpp"
+
+#include "logging/Logging.hpp"
+
+#include <stdint.h>
+#include <inttypes.h>
+#include <chrono>
+#include <sstream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <utility>
+#include <vector>
+
+/**
+ * @brief Name used by TRACE TLOG calls from this source file
+ */
+#define TRACE_NAME "NICReceiver" // NOLINT
+
+/**
+ * @brief TRACE debug levels used in this source file
+ */
+enum
+{
+  TLVL_ENTER_EXIT_METHODS = 5,
+  TLVL_WORK_STEPS = 10,
+  TLVL_BOOKKEEPING = 15
+};
+
+namespace dunedaq {
+namespace dpdklibs {
+
+NICReceiver::NICReceiver(const std::string& name)
+  : DAQModule(name)
+{
+  register_command("conf", &NICReceiver::do_configure);
+  register_command("start", &NICReceiver::do_start);
+  register_command("stop", &NICReceiver::do_stop);
+  register_command("scrap", &NICReceiver::do_scrap);
+}
+
+NICReceiver::~NICReceiver()
+{
+}
+
+void
+NICReceiver::init(const data_t& args)
+{
+}
+
+void
+NICReceiver::do_configure(const data_t& args)
+{
+}
+
+void
+NICReceiver::do_start(const data_t& args)
+{
+
+}
+
+void
+NICReceiver::do_stop(const data_t& args)
+{
+
+}
+
+void
+NICReceiver::do_scrap(const data_t& args)
+{
+}
+
+void
+NICReceiver::get_info(opmonlib::InfoCollector& ci, int level)
+{
+
+}
+
+} // namespace dpdklibs
+} // namespace dunedaq
+
+DEFINE_DUNE_DAQ_MODULE(dunedaq::dpdklibs::NICReceiver)
