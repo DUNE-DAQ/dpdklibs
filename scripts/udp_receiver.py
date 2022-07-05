6ac40c10b611d01239537c36c051409ad5a8b753
jmcarcell <jmcarcell@users.noreply.github.com>
2022-07-05 13:20:38 +0200

    Add a size argument and some fixes

--- scripts/udp_receiver.py
+++ scripts/udp_receiver.py
@@ -6,5 +6,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 s.bind(('0.0.0.0', 4444))
 
 while True:
-    data, address = s.recvfrom(4096)
-    print("Server received: ", data.decode('utf-8'), "\n")
+    data, address = s.recvfrom(20000)
+    print(f'Size of the message received: {len(data)}')
+    print('Message received: ', data.decode('utf-8'), '\n')
