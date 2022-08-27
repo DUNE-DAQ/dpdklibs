3d7304e942648c8fcd97ac200c81cf3fc1422a7f
jmcarcell <jmcarcell@users.noreply.github.com>
2022-08-27 18:41:13 +0200

    Add missing parameter to the configuration generation

--- python/dpdklibs/sender_confgen.py
+++ python/dpdklibs/sender_confgen.py
@@ -50,12 +50,13 @@ def generate(
 
     modules += [DAQModule(name="nic_sender", plugin="NICSender",
                           conf=nsc.Conf(
-                               eal_arg_list=EAL_ARGS,
-                               frontend_type='tde',
-                               number_of_cores=NUMBER_OF_CORES,
-                               burst_size=1,
-                               rate=1,
-                               core_list=core_maps,
+                              eal_arg_list=EAL_ARGS,
+                              frontend_type='tde',
+                              number_of_cores=NUMBER_OF_CORES,
+                              burst_size=1,
+                              rate=1,
+                              core_list=core_maps,
+                              time_tick_difference=TIME_TICK_DIFFERENCE,
                           )
                           
         )]
