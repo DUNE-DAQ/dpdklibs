29a40625683dd3debb126a9ca01808f18c5ea4de
eflumerf <61473357+eflumerf@users.noreply.github.com>
2022-12-12 15:44:38 -0600

    Revert "Eric's clang format fixes"

--- include/dpdklibs/CyclicDataGenerator.hpp
+++ include/dpdklibs/CyclicDataGenerator.hpp
@@ -1,29 +1,29 @@
 #ifndef CYCLICDATAGENERATOR_HPP_
 #define CYCLICDATAGENERATOR_HPP_
 
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 namespace dunedaq {
 namespace dpdklibs {
 namespace cyclicdatagenerator {
 
-class CyclicDataGenerator
-{
-public:
-  CyclicDataGenerator(size_t offset = 0);
-  CyclicDataGenerator(char* pattern, size_t offset = 0);
-  ~CyclicDataGenerator();
 
-  char get_next();
-  void get_next_n(char* dst, size_t n);
-  char get_prev();
-  void get_prev_n(char* dst, size_t n);
+class CyclicDataGenerator {
+public:
+    CyclicDataGenerator(size_t offset = 0);
+    CyclicDataGenerator(char *pattern, size_t offset = 0);
+    ~CyclicDataGenerator();
+
+    char get_next();
+    void get_next_n(char *dst, size_t n);
+    char get_prev();
+    void get_prev_n(char *dst, size_t n);
 
 private:
-  const char* pattern;
-  const size_t slice_len;
-  size_t current_slice_index = 0;
+    const char* pattern;
+    const size_t slice_len;
+    size_t current_slice_index = 0;
 };
 
 } // namespace cyclicdatagenerator
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,4 +1,3 @@
-void
-foo()
-{
+void foo() {
+
 }
