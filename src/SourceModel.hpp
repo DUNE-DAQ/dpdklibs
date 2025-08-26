b2ac3b229eb7afbd74ae487a9d4d574f7786e503
Marco Roda <mroda@liverpool.ac.uk>
2025-08-26 17:26:03 +0200

    Rename metric

--- src/SourceModel.hpp
+++ src/SourceModel.hpp
@@ -125,7 +125,7 @@ public:
 
     opmon::SourceInfo info;
     // RS FIXME: These are NOT dropped frames!!! Rename on opmon is needed
-    info.set_dropped_frames( m_failed_to_send_daq_payloads.exchange(0) ); 
+    info.set_failed_to_send_daq_payloads( m_failed_to_send_daq_payloads.exchange(0) ); 
 
     publish( std::move(info) );
   }
