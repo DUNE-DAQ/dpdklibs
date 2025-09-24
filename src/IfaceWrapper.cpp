fabf052b561464e22f11ce63ff7cde68b56f02c5
sbhuller <bhuller.shyam@gmail.com>
2025-09-24 14:35:31 +0200

    remove timestamp word from block length

--- src/IfaceWrapper.cpp
+++ src/IfaceWrapper.cpp
@@ -494,9 +494,10 @@ IfaceWrapper::handle_udp_payload(int src_rx_q, char* payload, std::size_t size)
   while (ptr + sizeof(dunedaq::detdataformats::DAQEthHeader) < end) { // Scatter loop start
     // Reinterpret directly to DAQEthHeader
     auto hdrp = reinterpret_cast<dunedaq::detdataformats::DAQEthHeader*>(ptr);
+    char* end_ptr = ptr + sizeof(dunedaq::detdataformats::DAQEthHeader) + data_bytes;
 
     // Check number of block words and do corrupt length check
-    unsigned block_words = unsigned(hdrp->block_length);
+    unsigned block_words = unsigned(hdrp->block_length) - 1; // removing timestamp word from the block length.
     if (block_words == 0 || block_words > m_max_block_words) {
       // corrupted length -> stop
       return;
@@ -506,7 +507,7 @@ IfaceWrapper::handle_udp_payload(int src_rx_q, char* payload, std::size_t size)
     std::size_t data_bytes = std::size_t(block_words) * sizeof(dunedaq::detdataformats::DAQEthHeader::word_t);
 
     // Check if full payload fits
-    if (ptr + sizeof(dunedaq::detdataformats::DAQEthHeader) + data_bytes > end) {
+    if (end_ptr > end) {
       // truncated payload -> stop, add opmon counter or warning
       return;
     }
