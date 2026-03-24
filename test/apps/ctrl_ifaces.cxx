10682f8617d19d546f29cdcca5ac4ffda6d21834
sbhuller <bhuller.shyam@gmail.com>
2026-03-24 13:25:56 +0100

    fixes to script, add eal init to pick up ports

--- test/apps/ctrl_ifaces.cxx
+++ test/apps/ctrl_ifaces.cxx
@@ -8,46 +8,53 @@
 #include "CLI/Config.hpp"
 #include "CLI/Formatter.hpp"
 
+#include "logging/Logging.hpp"
+
 #include <rte_memory.h>
 #include <rte_launch.h>
 #include <rte_eal.h>
 #include <rte_per_lcore.h>
 #include <rte_lcore.h>
 #include <rte_debug.h>
+#include <rte_common.h>
+#include <rte_ethdev.h>
+
 
 void ifaces_down() {
-  uint16_t portid
+  uint16_t portid;
   int ret;
 
   RTE_ETH_FOREACH_DEV(portid) {
     printf("bringing down port %d...", portid);
     ret = rte_eth_dev_stop(portid);
-    if (ret != 0)
-        rte_exit(EXIT_FAILURE, "rte_eth_dev_stop: err=%s, port=%u\n",
-              strerror(-ret), portid);
+    if (ret != 0) {      
+      rte_eal_cleanup();
+      rte_exit(EXIT_FAILURE, "rte_eth_dev_stop: err=%s, port=%u\n",
+            strerror(-ret), portid);
+    }
     rte_eth_dev_set_link_down(portid);
     printf(" Done\n");
   }
-  return 0  
 }
 
 void ifaces_up() {
-  uint16_t portid
+  uint16_t portid;
   int ret;
 
   RTE_ETH_FOREACH_DEV(portid) {
     printf("bringing up port %d...", portid);
     rte_eth_dev_set_link_up(portid);
     ret = rte_eth_dev_start(portid);
-    if (ret != 0)
-        rte_exit(EXIT_FAILURE, "rte_eth_dev_start: err=%s, port=%u\n",
-              strerror(-ret), portid);
+    if (ret != 0) {
+      rte_eal_cleanup();
+      rte_exit(EXIT_FAILURE, "rte_eth_dev_start: err=%s, port=%u\n",
+            strerror(-ret), portid);
+    }
     printf(" Done\n");
   }
-  return 0  
 }
 
-int main() {
+int main(int argc, char** argv) {
 
   bool up;
   bool down;
@@ -56,10 +63,24 @@ int main() {
   app.add_flag("-u,--up", up, "bring up all interfaces.");
   app.add_flag("-d,--down", down, "bring down all interfaces.");
 
+  CLI11_PARSE(app, argc, argv);
+  argc = 1; // So rte_eal_init doesn't interpret what gets passed to the command line
+
   if (down & up) {
-    printf("Can only specify one of the flags. Exiting...")
-    exit(1)
+    printf("Can only specify one of the flags. Exiting...");
+    exit(1);
   }
+
+
+  int retval = rte_eal_init(argc, argv);
+
+  if (retval < 0) {
+    rte_exit(EXIT_FAILURE, "ERROR: EAL initialization failed, info: %s\n", strerror(abs(retval)));
+  }
+
+  int nb_ports = rte_eth_dev_count_avail();
+  TLOG() << "There are " << nb_ports << " ethernet ports available out of a total of " << rte_eth_dev_count_total();
+
   if(down) {
     printf("bringing down all interfaces on server.");
     ifaces_down();
@@ -69,5 +90,5 @@ int main() {
     ifaces_up();
   }
 
-
+  rte_eal_cleanup();
 }
